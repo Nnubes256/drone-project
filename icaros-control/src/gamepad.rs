c9df519f615e4225e9c8a193c872989794815226
Ignacio <nnubes256@gmail.com>
2020-03-05 15:31:23 +0000

    Implemented additive throttle control system

--- icaros-control/src/gamepad.rs
+++ icaros-control/src/gamepad.rs
@@ -1,3 +1,4 @@
+use std::time::Instant;
 use icaros_base::utils::map_values;
 use icaros_base::comms::air::G2AControllerAxisState;
 use gilrs::ev::Code;
@@ -7,6 +8,9 @@ use gilrs::GamepadId;
 use std::error::Error;
 use gilrs::{Gilrs, MappingSource, Axis, Button};
 use gilrs::ev::filter::{Filter, deadzone};
+
+const THROTTLE_CHANGE_SPEED: f32 = 0.25;
+const THROTTLE_QUICK_TRIGGER_BOUND: f32 = 0.1;
 
 #[derive(Serialize, Deserialize, Clone)]
 #[serde(tag = "throttle_lock")]
@@ -83,7 +87,11 @@ impl RawAxisState {
     }
 
     pub fn set_throttle(&mut self, value: f32) {
-        self.throttle = f32::min(f32::max(value, -1.0), 1.0);
+        self.throttle = f32::min(f32::max(value, 0.0), 1.0);
+    }
+
+    pub fn add_throttle(&mut self, value: f32) {
+        self.set_throttle(self.throttle + value);
     }
 }
 
@@ -124,20 +132,28 @@ struct PhysicalGamepadAxesCodes {
     pub left_stick_y: Code,
     pub right_stick_x: Code,
     pub right_stick_y: Code,
+    pub left_trigger: Code,
+    pub right_trigger: Code,
     pub west_button: Code,
 }
 
 impl PhysicalGamepadAxesCodes {
-    pub fn new(left_stick_x: Code, left_stick_y: Code, right_stick_x: Code, right_stick_y: Code, west_button: Code) -> Self
+    pub fn new(
+        left_stick_x: Code, left_stick_y: Code,
+        right_stick_x: Code, right_stick_y: Code,
+        left_trigger: Code, right_trigger: Code,
+        west_button: Code) -> Self
     {
-        PhysicalGamepadAxesCodes { left_stick_x, left_stick_y, right_stick_x, right_stick_y, west_button }
+        PhysicalGamepadAxesCodes { left_stick_x, left_stick_y, right_stick_x, right_stick_y, left_trigger, right_trigger, west_button }
     }
 }
 
 pub struct PhysicalGamepadService {
     driver: Gilrs,
     current_gamepad: Option<GamepadId>,
-    axes_codes: Option<PhysicalGamepadAxesCodes>
+    axes_codes: Option<PhysicalGamepadAxesCodes>,
+    last_timestamp: Instant,
+    throttle_modifier: f32,
 }
 
 macro_rules! deadzone {
@@ -160,7 +176,9 @@ impl GamepadService for PhysicalGamepadService {
     }
 
     fn process(&mut self, state: &mut ControlState) -> Result<bool, Box<dyn Error>> {
-        while let Some(_) = self.driver.next_event().filter_ev(&deadzone, &mut self.driver) {};
+        while let Some(ev) = self.driver.next_event().filter_ev(&deadzone, &mut self.driver) {
+            //info!("{:?}", ev);
+        };
 
         if self.current_gamepad.is_none() {
             if let Some(id) = self.search_for_gamepad() {
@@ -185,6 +203,8 @@ impl GamepadService for PhysicalGamepadService {
                         gpad.axis_code(Axis::LeftStickY).unwrap(),
                         gpad.axis_code(Axis::RightStickX).unwrap(),
                         gpad.axis_code(Axis::RightStickY).unwrap(),
+                        gpad.button_code(Button::LeftTrigger2).unwrap(),
+                        gpad.button_code(Button::RightTrigger2).unwrap(),
                         gpad.button_code(Button::West).unwrap()
                     ));
                     self.axes_codes.as_ref().unwrap()
@@ -195,37 +215,51 @@ impl GamepadService for PhysicalGamepadService {
             let left_stick_y = gpad_state.axis_data(axes_codes.left_stick_y);
             let right_stick_x = gpad_state.axis_data(axes_codes.right_stick_x);
             let right_stick_y = gpad_state.axis_data(axes_codes.right_stick_y);
+            let left_trigger = gpad_state.button_data(axes_codes.left_trigger);
+            let right_trigger = gpad_state.button_data(axes_codes.right_trigger);
             let west_button = gpad_state.button_data(axes_codes.west_button);
 
             debug!(target: "gamepad", "{:?} {}", left_stick_x, self.driver.counter());
+
+            let delta_time = self.last_timestamp.elapsed().as_secs_f32();
+
+            self.throttle_modifier = 0.0;
 
             {
                 let axis_control_data = state.axes_mut();
 
                 if let Some(value) = left_stick_x {
                     if value.counter() >= self.driver.counter() {
-                        axis_control_data.set_yaw(deadzone!(-0.01, value.value(), 0.01))
+                        axis_control_data.set_yaw(deadzone!(-0.01, value.value(), 0.01));
                     }
                 }
 
                 if let Some(value) = left_stick_y {
-                    if value.counter() >= self.driver.counter() {
-                        axis_control_data.set_throttle(deadzone!(-0.01, value.value(), 0.01))
-                    }
+                    axis_control_data.add_throttle(deadzone!(-0.01, value.value(), 0.01) * delta_time * THROTTLE_CHANGE_SPEED);
+                }
+
+                if let Some(value) = left_trigger {
+                    self.throttle_modifier -= value.value() * THROTTLE_QUICK_TRIGGER_BOUND;
+                }
+
+                if let Some(value) = right_trigger {
+                    self.throttle_modifier += value.value() * THROTTLE_QUICK_TRIGGER_BOUND;
                 }
 
                 if let Some(value) = right_stick_x {
                     if value.counter() >= self.driver.counter() {
-                        axis_control_data.set_roll(deadzone!(-0.01, value.value(), 0.01))
+                        axis_control_data.set_roll(deadzone!(-0.01, value.value(), 0.01));
                     }
                 }
 
                 if let Some(value) = right_stick_y {
                     if value.counter() >= self.driver.counter() {
-                        axis_control_data.set_pitch(deadzone!(-0.01, value.value(), 0.01))
+                        axis_control_data.set_pitch(deadzone!(-0.01, value.value(), 0.01));
                     }
                 }
             }
+
+            self.last_timestamp = Instant::now();
 
             if let Some(value) = west_button {
                 if value.counter() >= self.driver.counter() {
@@ -269,9 +303,21 @@ impl GamepadService for PhysicalGamepadService {
     fn control_state_into_message_state(&mut self, msg_state: &mut G2AControllerAxisState, ctrl_state: &ControlState) {
         let axes = ctrl_state.axes_ref();
 
-        match ctrl_state.throttle_lock {
+        msg_state.throttle = f64::max(
+            0.0,
+            f64::min(
+                127.0,
+                map_values(
+                    (axes.get_throttle() + self.throttle_modifier) as f64,
+                    (0.0, 1.0),
+                    (0.0, 127.0)
+                )
+            )
+        ) as i8;
+
+        /*match ctrl_state.throttle_lock {
             ThrottleLockState::Unlocked => {
-                msg_state.throttle = f64::max(0.0, map_values(axes.get_throttle() as f64, (0.0, 1.0), (0.0, 127.0))) as i8;
+
             },
             ThrottleLockState::Locking { value } => {
                 msg_state.throttle = f64::max(0.0, map_values(value as f64, (0.0, 1.0), (0.0, 127.0))) as i8;
@@ -284,11 +330,11 @@ impl GamepadService for PhysicalGamepadService {
                     127.0),
                 0.0) as i8;
             }
-        }
+        }*/
 
         msg_state.roll = map_values(axes.get_roll() as f64, (-1.0, 1.0), (-128.0, 127.0)) as i8;
         msg_state.pitch = map_values(-axes.get_pitch() as f64, (-1.0, 1.0), (-128.0, 127.0)) as i8;
-        msg_state.yaw = map_values(axes.get_yaw() as f64, (-1.0, 1.0), (-128.0, 127.0)) as i8;
+        msg_state.yaw = map_values(-axes.get_yaw() as f64, (-1.0, 1.0), (-128.0, 127.0)) as i8;
     }
 }
 
@@ -297,7 +343,9 @@ impl PhysicalGamepadService {
         Ok(PhysicalGamepadService {
             driver: Gilrs::new()?,
             current_gamepad: None,
-            axes_codes: None
+            axes_codes: None,
+            last_timestamp: Instant::now(),
+            throttle_modifier: 0.0,
         })
     }
 
@@ -306,6 +354,10 @@ impl PhysicalGamepadService {
             if gamepad.mapping_source() != MappingSource::None {
                 info!("Found gamepad: {} - {} ({:?}, ff={}, {:?})", id, gamepad.name(),
                     gamepad.power_info(), gamepad.is_ff_supported(), gamepad.mapping_source());
+
+                if gamepad.is_ff_supported() {
+
+                }
                 return Some(id);
             }
 
